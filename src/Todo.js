5cb1f86a69830bb48ee24e0ab751196d9fb28414
sarveshzinios <sarvesh.singh@ziniostech.com>
2019-12-15 13:45:36 +0530

    chatbot

--- src/Todo.js
+++ src/Todo.js
@@ -103,12 +103,6 @@ export default class TodoList extends Component{
                     </div>
                 </nav>
                 <ul id="myUL">
-                    <li>Hit the gym</li>
-                    <li className="checked">Pay bills</li>
-                    <li>Meet George</li>
-                    <li>Buy eggs</li>
-                    <li>Read a book</li>
-                    <li>Organize office</li>
                     {
                        
                         
